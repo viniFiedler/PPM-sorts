1794d6c5630498954bbd5a51612ad52f7716d8bb
Vincius <fiedler.vinicius1@gmail.com>
2022-03-10 20:52:08 -0300

    Starting on creating the first image

--- projeto.c
+++ projeto.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include "image.h"
+#define BLACK 0, 0, 0
+
+struct Pixel image[ALTU_IMG][LARG_IMG];
+
+void criaImagemEscura(struct Pixel image[ALTU_IMG][LARG_IMG])
+{
+    int i,j;
+    struct Pixel fundo = {BLACK};
+    
+    for(i = 0; i < ALTU_IMG; i++)
+        {
+            for(j = 0; j < LARG_IMG; j++)
+            {
+                image[i][j] = fundo;
+            }
+        }
+}
 
 int main()
-{
-    printf(" %d ",soma(3));   
+{   
+    criaImagemEscura(image);
+    createPPM("Teste Preto",image);
 }
